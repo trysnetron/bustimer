73fe065fb7d8d26a1bcd554c218085a8f142ee91
Trym Sneltvedt <trysne@gmail.com>
2019-05-23 22:28:17 +0200

    Add support for nex days bus times

--- src/js/bustimer.js
+++ src/js/bustimer.js
@@ -18,6 +18,7 @@ function prettyTime(simpleTime) {
 function timeToNext(hour, minute, second, table) {
   const from = simpleTime(hour, minute, second);
   return table
+    .concat(table.map(t => [t[0] + 24, t[1]]))
     .map(t => simpleTime(t[0], t[1]) - from)
     .filter(t => t >= 0)
     .sort((a, b) => a > b)
