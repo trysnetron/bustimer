4f5aeaf1174d9eefa0f1672548967bf3d1fb34ef
Trym Sneltvedt <trysne@gmail.com>
2019-05-23 22:37:19 +0200

    Separate dom manips and app mounting from app logic

--- src/js/bustimer.js
+++ src/js/bustimer.js
@@ -15,7 +15,7 @@ function prettyTime(simpleTime) {
   );
 }
 
-function timeToNext(hour, minute, second, table) {
+export function timeToNext(hour, minute, second, table) {
   const from = simpleTime(hour, minute, second);
   return table
     .concat(table.map(t => [t[0] + 24, t[1]]))
@@ -23,44 +23,4 @@ function timeToNext(hour, minute, second, table) {
     .filter(t => t >= 0)
     .sort((a, b) => a > b)
     .map(t => prettyTime(t));
-}
-
-export default function createBustimer(root, stops) {
-  const rootElem = document.querySelector(root);
-  if (!rootElem) throw new Error(`Root element '${root}' not found`);
-
-  const updateFuncs = stops.map(stop => {
-    const mainElem = document.createElement("div");
-    mainElem.classList.add("stop");
-
-    const nameElem = document.createElement("h2");
-    nameElem.classList.add("stopname");
-    nameElem.innerText = stop.name;
-    mainElem.appendChild(nameElem);
-
-    const timeElem = document.createElement("div");
-    timeElem.classList.add("timeleft");
-    mainElem.appendChild(timeElem);
-
-    const nextElem = document.createElement("div");
-    mainElem.appendChild(nextElem);
-
-    rootElem.appendChild(mainElem);
-
-    return function update(hour, minute, second) {
-      const next = timeToNext(hour, minute, second, stop.times);
-      timeElem.innerText = next[0];
-      nextElem.innerText = `Next: ${next[1]}`;
-    };
-  });
-
-  function update() {
-    const now = new Date();
-    updateFuncs.forEach(f =>
-      f(now.getHours(), now.getMinutes(), now.getSeconds())
-    );
-  }
-
-  update();
-  setInterval(update, 1000);
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,42 @@
-import createBustimer from "./bustimer";
+import { timeToNext } from "./bustimer";
 import stops from "./stops";
 
-createBustimer("#app", stops);
+(function mountApp(root, stops) {
+  const rootElem = document.querySelector(root);
+  if (!rootElem) throw new Error(`Root element '${root}' not found`);
+
+  const updateFuncs = stops.map(stop => {
+    const mainElem = document.createElement("div");
+    mainElem.classList.add("stop");
+
+    const nameElem = document.createElement("h2");
+    nameElem.classList.add("stopname");
+    nameElem.innerText = stop.name;
+    mainElem.appendChild(nameElem);
+
+    const timeElem = document.createElement("div");
+    timeElem.classList.add("timeleft");
+    mainElem.appendChild(timeElem);
+
+    const nextElem = document.createElement("div");
+    mainElem.appendChild(nextElem);
+
+    rootElem.appendChild(mainElem);
+
+    return function update(hour, minute, second) {
+      const next = timeToNext(hour, minute, second, stop.times);
+      timeElem.innerText = next[0];
+      nextElem.innerText = `Next: ${next[1]}`;
+    };
+  });
+
+  function update() {
+    const now = new Date();
+    updateFuncs.forEach(f =>
+      f(now.getHours(), now.getMinutes(), now.getSeconds())
+    );
+  }
+
+  update();
+  setInterval(update, 1000);
+})("#app", stops);
