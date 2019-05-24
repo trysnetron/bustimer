febbea68af813e6d1e280adc54701d455414519e
Trym Sneltvedt <trysne@gmail.com>
2019-05-24 08:41:29 +0200

    Add limit to timeToNext responses

--- src/js/bustimer.js
+++ src/js/bustimer.js
@@ -15,12 +15,13 @@ function prettyTime(simpleTime) {
   );
 }
 
-export function timeToNext({ h, m, s }, times) {
+export function timeToNext({ h, m, s }, times, max = 3) {
   const from = simpleTime(h, m, s);
   return times
     .concat(times.map(t => [t[0] + 24, t[1]]))
     .map(t => simpleTime(t[0], t[1]) - from)
     .filter(t => t >= 0)
     .sort((a, b) => a > b)
+    .filter((t, i) => i < max)
     .map(t => prettyTime(t));
 }
--- tests/bustimer.test.js
+++ tests/bustimer.test.js
@@ -12,3 +12,18 @@ test('returns time to next bus departure', () => {
   expect(res[1]).toBe('05:30:00');
   expect(res[2]).toBe('23:30:00');
 });
+
+test('defaults works', () => {
+  const res = timeToNext({ h: 12, m: 30, s: 0 }, testTimes);
+  expect(res.length).toBe(3);
+  expect(res[0]).toBe('00:30:00');
+  expect(res[1]).toBe('05:30:00');
+  expect(res[2]).toBe('23:30:00');
+});
+
+test('limits response length', () => {
+  const res = timeToNext({ h: 12, m: 30, s: 0 }, testTimes, 2);
+  expect(res.length).toBe(2);
+  expect(res[0]).toBe('00:30:00');
+  expect(res[1]).toBe('05:30:00');
+});
