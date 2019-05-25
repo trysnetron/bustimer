c6b6dd1f1fd043d2e2bee83b274a812cde04fcd8
Trym Sneltvedt <trysne@gmail.com>
2019-05-25 16:16:11 +0200

    Add prettier styling to tests

--- tests/bustimer.test.js
+++ tests/bustimer.test.js
@@ -1,48 +1,44 @@
-import { timeToNext } from '../src/js/bustimer';
+import { timeToNext } from "../src/js/bustimer";
 
 const testTable = [
   {
     wkdays: [1, 2, 3, 4, 5],
-    times: [
-      [12, 0],
-      [13, 0],
-      [18, 0]
-    ]
+    times: [[12, 0], [13, 0], [18, 0]]
   },
   {
     wkdays: [0, 6],
     times: []
   }
-]
+];
 
-test('returns time to next bus departure', () => {
+test("returns time to next bus departure", () => {
   const res = timeToNext({ h: 12, m: 30, s: 0, wkday: 1 }, testTable);
-  expect(res[0]).toBe('00:30:00');
-  expect(res[1]).toBe('05:30:00');
-  expect(res[2]).toBe('23:30:00');
+  expect(res[0]).toBe("00:30:00");
+  expect(res[1]).toBe("05:30:00");
+  expect(res[2]).toBe("23:30:00");
 });
 
-test('defaults works', () => {
+test("defaults works", () => {
   const res = timeToNext({ h: 12, m: 30, s: 0, wkday: 1 }, testTable);
   expect(res.length).toBe(3);
-  expect(res[0]).toBe('00:30:00');
-  expect(res[1]).toBe('05:30:00');
-  expect(res[2]).toBe('23:30:00');
+  expect(res[0]).toBe("00:30:00");
+  expect(res[1]).toBe("05:30:00");
+  expect(res[2]).toBe("23:30:00");
 });
 
-test('limits response length', () => {
+test("limits response length", () => {
   const res = timeToNext({ h: 12, m: 30, s: 0, wkday: 1 }, testTable, 2);
   expect(res.length).toBe(2);
-  expect(res[0]).toBe('00:30:00');
-  expect(res[1]).toBe('05:30:00');
+  expect(res[0]).toBe("00:30:00");
+  expect(res[1]).toBe("05:30:00");
 });
 
-test('takes different weekdays into account', () => {
-  const res = timeToNext({ h: 12, m: 30, s: 0, wkday: 0}, testTable);
+test("takes different weekdays into account", () => {
+  const res = timeToNext({ h: 12, m: 30, s: 0, wkday: 0 }, testTable);
   expect(res[0]).toBe("23:30:00");
 });
 
-test('returns no times for consecutive empty days', () => {
+test("returns no times for consecutive empty days", () => {
   const res = timeToNext({ h: 12, m: 30, s: 0, wkday: 6 }, testTable);
   expect(res.length).toBe(0);
 });
